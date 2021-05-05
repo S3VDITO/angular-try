bc45f5308f77ddd6778e1022a6a18e313758a84f
Evgeniy Chepack <S3VDIT0@yandex.ru>
2021-05-05 18:09:52 +0300

    DI реализован

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +10,20 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor() { }
-
+  constructor(private heroService: HeroService) {}
   selectedHero?: Hero;
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(hero => this.heroes = hero);
+  }
+
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
