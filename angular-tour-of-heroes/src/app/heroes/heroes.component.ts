420ce378be3a97e58c6465ee5c877d324f9a85b9
Evgeniy Chepack <S3VDIT0@yandex.ru>
2021-05-05 14:45:24 +0300

    Display a List

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -12,10 +12,6 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
